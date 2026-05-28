bf729fb7d8d623199763171990766c01b7521e21
ORAB-XK-2007 <duybao080707@gmail.com>
2026-05-28 21:54:02 +0700

    update
    thêm chức năng hiển thị thông tin

---
+++ Manage.c
@@ -0,0 +1,226 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#define MAX 100
+
+typedef struct {
+    char fullName[MAX];
+    char cccd[MAX];
+    char birthYear[MAX];
+    char province[MAX];
+    char phoneNumber[MAX];
+} Manager;
+
+Manager manager;
+
+//	Hàm hiển thị thông tin
+void showManagerInfo() {
+
+    printf("\n========== THÔNG TIN QUẢN LÝ ==========\n");
+
+    printf("Họ tên       : %s\n", manager.fullName);
+    printf("CCCD         : %s\n", manager.cccd);
+    printf("Năm sinh     : %s\n", manager.birthYear);
+    printf("Quê quán     : %s\n", manager.province);
+    printf("Số điện thoại: %s\n", manager.phoneNumber);
+}
+
+//	Hàm chỉnh sửa thông tin
+void editManagerAccount() {
+    printf("\n[Chỉnh sửa thông tin tài khoản quản lý]\n");
+}
+
+//	Hàm tạo tài khoản cho cư dân
+void createResidentAccount() {
+    printf("\n[Tạo tài khoản cho cư dân]\n");
+}
+
+//	Hàm chỉnh sửa thông tin tài khoản cư dân
+void editResidentAccount() {
+    printf("\n[Chỉnh sửa thông tin tài khoản cư dân]\n");
+}
+
+//	Hàm xóa tài khoản cư dân
+void deleteResidentAccount() {
+    printf("\n[Xóa tài khoản cư dân]\n");
+}
+
+//	Hàm hiển thị danh sách các phòng
+void showRoomList() {
+    printf("\n[Xem danh sách các phòng]\n");
+}
+
+//	Hàm tìm kiếm cư dân
+void searchResident() {
+    printf("\n[Tìm kiếm cư dân]\n");
+}
+
+//	Hàm nhập số điện, số nước
+void inputElectricWater() {
+    printf("\n[Nhập thông tin số điện, số nước]\n");
+}
+
+//	Hàm hiển thị danh sách hóa đơn
+void showMonthlyBills() {
+    printf("\n[Xem danh sách hóa đơn theo tháng]\n");
+}
+
+//	Hàm đăng nhập
+int login() {
+
+    char username[MAX], password[MAX], path[200], filePassword[MAX];
+
+    FILE *file;
+
+    printf("=============== ĐĂNG NHẬP ===============\n");
+
+    printf("Nhập tên tài khoản: ");
+    scanf("%s", username);
+
+    printf("Nhập mật khẩu: ");
+    scanf("%s", password);
+
+    // Tạo đường dẫn file
+    sprintf(path, "Data/Account/Manage/%s.txt", username);
+
+    // Mở file
+    file = fopen(path, "r");
+
+    if(file == NULL) {
+        printf("\nKhông tìm thấy tài khoản!\n");
+        return 0;
+    }
+
+    // Đọc mật khẩu dòng đầu tiên
+    fgets(filePassword, MAX, file);
+
+    // Xóa '\n'
+    filePassword[strcspn(filePassword, "\n")] = 0;
+
+    // Kiểm tra mật khẩu
+    if(strcmp(password, filePassword) != 0) {
+
+        printf("\nSai mật khẩu!\n");
+
+        fclose(file);
+
+        return 0;
+    }
+
+    // Đọc thông tin dòng 2 -> 6
+    fgets(manager.fullName, MAX, file);
+    fgets(manager.cccd, MAX, file);
+    fgets(manager.birthYear, MAX, file);
+    fgets(manager.province, MAX, file);
+    fgets(manager.phoneNumber, MAX, file);
+
+    // Xóa '\n'
+    manager.fullName[strcspn(manager.fullName, "\n")] = 0;
+    manager.cccd[strcspn(manager.cccd, "\n")] = 0;
+    manager.birthYear[strcspn(manager.birthYear, "\n")] = 0;
+    manager.province[strcspn(manager.province, "\n")] = 0;
+    manager.phoneNumber[strcspn(manager.phoneNumber, "\n")] = 0;
+
+    fclose(file);
+
+    printf("\nĐăng nhập thành công!\n");
+
+    return 1;
+}
+
+void manageMenu()
+{
+	int choice;
+
+    do {
+
+        system("cls"); 
+
+        printf("====================================================\n");
+        printf("                    QUẢN LÝ                        \n");
+        printf("====================================================\n");
+
+        printf("1. Hiển thị thông tin quản lý\n");
+        printf("2. Chỉnh sửa thông tin tài khoản quản lý\n");
+        printf("3. Tạo tài khoản cho cư dân\n");
+        printf("4. Chỉnh sửa thông tin tài khoản cư dân\n");
+        printf("5. Xóa tài khoản cư dân\n");
+        printf("6. Xem danh sách các phòng\n");
+        printf("7. Tìm kiếm cư dân\n");
+        printf("8. Nhập thông tin số điện, số nước\n");
+        printf("9. Xem danh sách hóa đơn theo tháng\n");
+        printf("0. Thoát\n");
+
+        printf("====================================================\n");
+        printf("Nhập lựa chọn của bạn: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+
+            case 1:
+                showManagerInfo();
+                break;
+
+            case 2:
+                editManagerAccount();
+                break;
+
+            case 3:
+                createResidentAccount();
+                break;
+
+            case 4:
+                editResidentAccount();
+                break;
+
+            case 5:
+                deleteResidentAccount();
+                break;
+
+            case 6:
+                showRoomList();
+                break;
+
+            case 7:
+                searchResident();
+                break;
+
+            case 8:
+                inputElectricWater();
+                break;
+
+            case 9:
+                showMonthlyBills();
+                break;
+
+            case 0:
+                printf("Đang thoát chương trình...\n");
+                break;
+
+            default:
+                printf("Lựa chọn không hợp lệ!\n");
+        }
+
+        if(choice != 0) {
+            printf("\nNhấn Enter để tiếp tục...");
+            getchar();
+            getchar();
+        }
+
+    } while(choice != 0);
+}
+int main() {
+
+    SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	// Đăng nhập
+	if(login() == 0) {
+   		printf("\nĐăng nhập thất bại!\n");
+    	return 0;
+	}
+
+	//	Menu
+	manageMenu();
+}
